4a1f579608cd1dcfe61bd56f11110ae169a937fc
Jessie <chenjiaxin@bixin.cn>
2021-11-19 17:32:10 +0800

    feat: pre-commit优化

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 124, // 代码宽度建议不超过124字符
+  tabWidth: 2, // tab缩进2个空格
+  semi: false, // 末尾分号
+  singleQuote: true, // 单引号
+  jsxSingleQuote: true, // jsx中使用单引号
+  arrowParens: 'avoid', // 箭头函数仅在必要时使用()
+  htmlWhitespaceSensitivity: 'css', // html空格敏感度
+  useTabs: false,
+  trailingComma: "none",// 尾随逗号
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: "lf",
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin"); // webpack5自带，无需安装
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); //优化、压缩 CSS
+const PurgeCSSPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = merge(common, {
@@ -9,25 +13,64 @@ module.exports = merge(common, {
   entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name]-[chunkhash:8].js",
+    filename: "[name].[chunkhash:8].js",
     clean: true, // 编译前清除目录
   },
   module: {
     rules: [
       {
         test: /\.s?css$/,
-        include: [path.resolve(__dirname, "src/common/styles"), /node_modules/],
-        use: [MiniCssExtractPlugin.loader],
+        // exclude: [paths.css, /node_modules/],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
     ],
   },
   plugins: [
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin(), // 打包体积分析
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
+      // 提取 CSS
+      filename: "[name].[chunkhash:8].css",
+    }),
+    new PurgeCSSPlugin({ // 去除无用的CSS代码
+      paths: glob.sync(`${path.resolve(__dirname, "src")}`, {
+        nodir: true,
+      }),
     }),
   ],
-  optimization:{
-      minimizer:[]
-  }
+  optimization: {
+    runtimeChunk: true,// 运行时代码创建一个额外的 chunk，减少 entry chunk 体积，提高性能
+    // js压缩
+    minimizer: [
+      new TerserPlugin({
+        parallel: 4, // 使用多进程并发运行压缩以提高构建速度。
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin({
+        parallel: 4,
+      }),
+    ],
+  },
 });
